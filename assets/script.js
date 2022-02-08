12f0254728bf425e58ad3c6d8790f940b2296aa7
Nicholas Chinna Ystad <nicholasystad08@gmail.com>
2022-02-08 10:00:19 -0600

    All cookie buttons work

--- assets/script.js
+++ assets/script.js
@@ -18,25 +18,34 @@ credit.textContent = `Created by ${yourName}`
 
 
 //Gingerbread-----------------------------------------------------------------------------------------------
-// plus listener
+// + LISTEN
 gbPlusBtn.addEventListener('click', function() {
 console.log('Gingerbread + button was clicked!')
 })
 gbPlusBtn.addEventListener('click', function () {
     gb++;
     console.log('gb++', gb);
+// ORDER SUMMARY + 
+    const gbPlusBtn = document.querySelector('#add-gb');
+const gbValue = document.querySelector('#qty-gb');
+gbPlusBtn.addEventListener('click', function () {
+  gbValue.innerText = gb;
+});
   });
-
-
-// minus listener
+// - LISTEN
 gbMinusBtn.addEventListener('click', function() {
-
     console.log('Gingerbread - button was clicked!')
     })
 gbMinusBtn.addEventListener('click', function () {
         gb--;
         console.log('gb--', gb);
-      });
+      
+        const gbMinusBtn = document.querySelector('#minus-gb');
+        const gbValue = document.querySelector('#qty-gb');
+        gbMinusBtn.addEventListener('click', function () {
+          gbValue.innerText = gb;
+        });
+    });
 
 
 //Chocolate chip---------------------------------------------------------------------------------------------
@@ -47,6 +56,12 @@ ccPlusBtn.addEventListener('click', function() {
     ccPlusBtn.addEventListener('click', function () {
         cc++;
         console.log('cc++', cc);
+        
+        const ccPlusBtn = document.querySelector('#add-cc');
+const ccValue = document.querySelector('#qty-cc');
+ccPlusBtn.addEventListener('click', function () {
+  ccValue.innerText = cc;
+});
       });
     
     
@@ -58,6 +73,12 @@ ccPlusBtn.addEventListener('click', function() {
     ccMinusBtn.addEventListener('click', function () {
             cc--;
             console.log('cc--', cc);
+
+            const ccMinusBtn = document.querySelector('#minus-cc');
+            const ccValue = document.querySelector('#qty-cc');
+            ccMinusBtn.addEventListener('click', function () {
+              ccValue.innerText = cc;
+            });
           });
 
 // Sugar Cookies
@@ -68,6 +89,12 @@ sugarPlusBtn.addEventListener('click', function() {
     sugarPlusBtn.addEventListener('click', function () {
         sugar++;
         console.log('sugar++', sugar);
+
+        const sugarPlusBtn = document.querySelector('#add-sugar');
+        const sugarValue = document.querySelector('#qty-sugar');
+        sugarPlusBtn.addEventListener('click', function () {
+          sugarValue.innerText = sugar;
+        });
       });
     
     
@@ -79,4 +106,10 @@ sugarPlusBtn.addEventListener('click', function() {
     sugarMinusBtn.addEventListener('click', function () {
             sugar--;
             console.log('sugar--', sugar);
+
+            const sugarMinusBtn = document.querySelector('#minus-sugar');
+            const sugarValue = document.querySelector('#qty-sugar');
+            sugarMinusBtn.addEventListener('click', function () {
+              sugarValue.innerText = sugar;
+            });
           });
