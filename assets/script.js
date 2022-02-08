a871a81473a1dfe994132eeba157d50654f8b5db
Nicholas Chinna Ystad <nicholasystad08@gmail.com>
2022-02-07 21:42:34 -0600

    numbers are tracking

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,82 @@
-// HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
+let yourName = "Nic Ystad" 
+// cookie count
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-// selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
 const gbPlusBtn = document.querySelector('#add-gb')
+const gbMinusBtn = document.querySelector('#minus-gb')
+const ccPlusBtn = document.querySelector('#add-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
 
-// Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
+
+//Gingerbread-----------------------------------------------------------------------------------------------
+// plus listener
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
 console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 })
+gbPlusBtn.addEventListener('click', function () {
+    gb++;
+    console.log('gb++', gb);
+  });
 
-// TODO: Hook up event listeners for the rest of the buttons
+
+// minus listener
+gbMinusBtn.addEventListener('click', function() {
+
+    console.log('Gingerbread - button was clicked!')
+    })
+gbMinusBtn.addEventListener('click', function () {
+        gb--;
+        console.log('gb--', gb);
+      });
+
+
+//Chocolate chip---------------------------------------------------------------------------------------------
+// plus listener
+ccPlusBtn.addEventListener('click', function() {
+    console.log('Chocolate Chip + button was clicked!')
+    })
+    ccPlusBtn.addEventListener('click', function () {
+        cc++;
+        console.log('cc++', cc);
+      });
+    
+    
+    // minus listener
+    ccMinusBtn.addEventListener('click', function() {
+    
+        console.log('Chocolate Chip - button was clicked!')
+        })
+    ccMinusBtn.addEventListener('click', function () {
+            cc--;
+            console.log('cc--', cc);
+          });
+
+// Sugar Cookies
+// plus listener
+sugarPlusBtn.addEventListener('click', function() {
+    console.log('Sugar Cookie + button was clicked!')
+    })
+    sugarPlusBtn.addEventListener('click', function () {
+        sugar++;
+        console.log('sugar++', sugar);
+      });
+    
+    
+    // minus listener
+    sugarMinusBtn.addEventListener('click', function() {
+    
+        console.log('Sugar Cookie - button was clicked!')
+        })
+    sugarMinusBtn.addEventListener('click', function () {
+            sugar--;
+            console.log('sugar--', sugar);
+          });
